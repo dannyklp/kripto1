fc5fbec10fa5caa349feafe83ecf038925011072
dannyklp <35611956+dannyklp@users.noreply.github.com>
2018-01-19 23:38:14 +0300

    Update RSA.cpp

--- RSA.cpp
+++ RSA.cpp
@@ -1,68 +1,75 @@
 #include<iostream>
 #include<math.h>
+#include<locale>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 //to find gcd
 int gcd(int a, int h)
 {
-    int temp;
-    while(1)
-    {
-        temp = a%h;
-        if(temp==0)
-        return h;
-        a = h;
-        h = temp;
-    }
+	int temp;
+	while (1)
+	{
+		temp = a%h;
+		if (temp == 0)
+			return h;
+		a = h;
+		h = temp;
+	}
 }
 
 int main()
 {
-    //2 random prime numbers
-    double p = 3;
-    double q = 7;
-    double n=p*q;
-    double count;
-    double totient = (p-1)*(q-1);
+	setlocale(0, "Rus");
+	//2 random prime numbers
+	double p = 3;
+	double q = 7;
+	double n = p*q;
+	double count;
+	double totient = (p - 1)*(q - 1);
 
-    //public key
-    //e stands for encrypt
-    double e=2;
+	//public key
+	//e stands for encrypt
+	double e = 2;
 
-    //for checking co-prime which satisfies e>1
-    while(e<totient){
-    count = gcd(e,totient);
-    if(count==1)
-        break;
-    else
-        e++;
-    }
+	//for checking co-prime which satisfies e>1
+	while (e<totient) {
+		count = gcd(e, totient);
+		if (count == 1)
+			break;
+		else
+			e++;
+	}
 
-    //private key
-    //d stands for decrypt
-    double d;
+	//private key
+	//d stands for decrypt
+	double d;
 
-    //k can be any arbitrary value
-    double k = 2;
+	//k can be any arbitrary value
+	double k = 2;
 
-    //choosing d such that it satisfies d*e = 1 + k * totient
-    d = (1 + (k*totient))/e;
-    double msg = 12;
-    double c = pow(msg,e);
-    double m = pow(c,d);
-    c=fmod(c,n);
-    m=fmod(m,n);
+	//choosing d such that it satisfies d*e = 1 + k * totient
+	d = (1 + (k*totient)) / e;
+	cout << "Введите сообщение(число): ";
+	double msg;
+	cin >> msg;
 
-    cout<<"Message data = "<<msg;
-    cout<<"\n"<<"p = "<<p;
-    cout<<"\n"<<"q = "<<q;
-    cout<<"\n"<<"n = pq = "<<n;
-    cout<<"\n"<<"totient = "<<totient;
-    cout<<"\n"<<"e = "<<e;
-    cout<<"\n"<<"d = "<<d;
-    cout<<"\n"<<"Encrypted data = "<<c;
-    cout<<"\n"<<"Original Message sent = "<<m;
+	double c = pow(msg, e);
+	double m = pow(c, d);
+	c = fmod(c, n);
+	m = fmod(m, n);
 
-    return 0;
+	cout << "Message data = " << msg;
+	cout << "\n" << "p = " << p;
+	cout << "\n" << "q = " << q;
+	cout << "\n" << "n = pq = " << n;
+	cout << "\n" << "totient = " << totient;
+	cout << "\n" << "e = " << e;
+	cout << "\n" << "d = " << d;
+	cout << "\n" << "Encrypted data = " << c;
+	cout << "\n" << "Original Message sent = " << m;
+	system("pause");
+	return 0;
 }
